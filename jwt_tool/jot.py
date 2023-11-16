d87ab6e7fdef3793c8ff2cd8a6654fd72800653a
errbufferoverfl <gitlab@errbufferoverfl.me>
2023-11-17 08:05:49 +1100

    JWKS class to encapsulate multiple keys

--- jwt_tool/jot.py
+++ jwt_tool/jot.py
@@ -1,6 +1,6 @@
 import base64
 import enum
-from typing import Union
+from typing import Union, List
 
 from Cryptodome.PublicKey import RSA, ECC
 
@@ -185,15 +185,6 @@ class JWK:
         """
         self._e = base64.urlsafe_b64encode(value.e.to_bytes(3, byteorder="big"))
 
-    def sign_token(self, signing_algorithm: SigningAlgorithm):
-        pass
-
-    def verify_token(self, signing_algorithm: SigningAlgorithm):
-        pass
-
-    def generate_public_key_from_jwks(self, key_format: SymmetricKeyAlgorithm):
-        pass
-
     def _guess_key_type(self, public_key: bytes):
         """
         Guesses the key type based on the provided public key and sets the internal key and key type attributes.
@@ -257,3 +248,68 @@ class JWK:
 
     def __ne__(self, other):
         return not self.__eq__(other)
+
+
+class JWKS:
+    """
+    JSON Web Key Set (JWKS) representation.
+
+    Attributes:
+        keys (List[JWK]): List of JSON Web Keys in the set.
+
+    Methods:
+        add_key: Adds a JSON Web Key to the set.
+        get_key_by_kid: Retrieves a key from the set based on the Key ID (kid).
+        __str__: Returns a plain text representation of the JWKS.
+    """
+
+    def __init__(self, keys: List[JWK] = None):
+        """
+        Initializes a JWKS object.
+
+        Args:
+            keys (List[JWK]): List of JSON Web Keys. Defaults to an empty list.
+
+        Returns:
+            None
+        """
+        self.keys = keys or []
+
+    def add_key(self, key: JWK):
+        """
+        Adds a JSON Web Key to the set.
+
+        Args:
+            key (JWK): The JSON Web Key to add.
+
+        Returns:
+            None
+        """
+        self.keys.append(key)
+
+    def get_key_by_kid(self, kid: str) -> Union[JWK, None]:
+        """
+        Retrieves a key from the set based on the Key ID (kid).
+
+        Args:
+            kid (str): The Key ID (kid) to look for.
+
+        Returns:
+            Union[JWK, None]: The matching key if found, or None if not found.
+        """
+        for key in self.keys:
+            if key.kid == kid:
+                return key
+        return None
+
+    def __str__(self):
+        """
+        Returns a plain text representation of the JWKS.
+
+        Returns:
+            str: Plain text representation of the JWKS.
+        """
+        jwks_str = "JSON Web Key Set (JWKS):\n"
+        for key in self.keys:
+            jwks_str += str(key) + "\n"
+        return jwks_str
