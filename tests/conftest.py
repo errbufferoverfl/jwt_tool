47dea0db0d78fb5d972577b29a5cc4979b6a2eb1
errbufferoverfl <gitlab@errbufferoverfl.me>
2023-11-17 09:20:31 +1100

    Turn RSA and ECC keys into pytest fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture
+def rsa_public_key() -> str:
+    rsa_key_path = Path("data/rsa_public_key.pem")
+    with open(rsa_key_path, "rb") as file:
+        return file.read().decode("UTF-8")
+
+
+@pytest.fixture
+def ecc_public_key() -> str:
+    ecc_key_path = Path("data/ecc_public_key.pem")
+    with open(ecc_key_path, "rb") as file:
+        return file.read().decode("UTF-8")
--- tests/jwk_tests.py
+++ tests/jwk_tests.py
@@ -1,7 +1,4 @@
-from pathlib import Path
-
 import pytest
-from Cryptodome.PublicKey import RSA, ECC
 
 from jwt_tool.jot import JWK, SymmetricKeyAlgorithm, JWKS
 
@@ -21,45 +18,3 @@ class TestJWK:
         unknown_key = b'\x00\x01\x02\x03'  # Some arbitrary bytes
         with pytest.raises(Exception, match="Unknown key type"):
             jwks = JWK(kid="unknown", public_key=unknown_key)
-
-
-def test_jwks_add_key():
-    jwks = JWKS()
-    assert len(jwks.keys) == 0
-
-    jwk = JWK(kid="test_key", public_key="...")
-    jwks.add_key(jwk)
-
-    assert len(jwks.keys) == 1
-    assert jwks.keys[0] == jwk
-
-
-def test_jwks_get_key_by_kid():
-    jwks = JWKS()
-    jwk1 = JWK(kid="key1", public_key="...")
-    jwk2 = JWK(kid="key2", public_key="...")
-    jwk3 = JWK(kid="key3", public_key="...")
-
-    jwks.add_key(jwk1)
-    jwks.add_key(jwk2)
-    jwks.add_key(jwk3)
-
-    assert jwks.get_key_by_kid("key2") == jwk2
-    assert jwks.get_key_by_kid("nonexistent_key") is None
-
-
-def test_jwks_str():
-    jwks = JWKS()
-    jwk1 = JWK(kid="key1", public_key="...")
-    jwk2 = JWK(kid="key2", public_key="...")
-
-    jwks.add_key(jwk1)
-    jwks.add_key(jwk2)
-
-    expected_str = (
-        "JSON Web Key Set (JWKS):\n"
-        f"{str(jwk1)}\n"
-        f"{str(jwk2)}\n"
-    )
-
-    assert str(jwks) == expected_str
