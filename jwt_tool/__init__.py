ec386fd639717fccb98ca84855e0b308a2cc7e8b
errbufferoverfl <gitlab@errbufferoverfl.me>
2023-11-23 12:17:09 +1100

    Moves global enums to different file Is this a Python anti-pattern? Who knows, but it works right now

--- jwt_tool/__init__.py
+++ jwt_tool/__init__.py
@@ -1,3 +1,16 @@
+import enum
 import pathlib
 
 CONFIG_PATH = pathlib.Path(pathlib.Path.home() / ".config" / "jwt_tool")
+
+
+class SymmetricKeyAlgorithm(enum.Enum):
+    RSA = 0
+    ECC = 1
+    DSA = 2
+
+
+class SigningAlgorithm(enum.Enum):
+    RSA = 0
+    ECC = 1
+    HMAC = 2
