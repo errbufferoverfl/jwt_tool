489ff01c19d3b52cf99a008c34985ae0a73603f3
errbufferoverfl <gitlab@errbufferoverfl.me>
2023-12-01 20:22:55 +1100

    Adds unit tests for updating the payload, updating the header, updating the payload without signing changes

--- tests/jwt_tests.py
+++ tests/jwt_tests.py
@@ -1,49 +1,37 @@
+import copy
 import pytest
 
-from jwt_tool.JWT import Payload, Header, JWT, Signature
+from jwt_tool.JWT import Payload, Header, JWT, SigningConfig
 
 
 class TestJWT:
     def test_encoding_and_decoding(self):
         """
-        Scenario: JWT Encoding and Decoding
+        Scenario: Encoding and Decoding
 
         Given a JWT instance with a valid header, payload, and signature.
         When encoding the JWT.
         Then the encoded token should be a non-empty string.
         And When decoding the encoded token.
-        And When decoding the encoded token.
         Then the decoded payload should match the original payload.
         """
         expected_header = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
-
         header = Header("HS256", "JWT")
-        given_header = header.urlsafe_b64encode()
-
-        assert given_header == expected_header
+        given_header = header.encode()
+        assert given_header == expected_header, f"Expected header: {expected_header}, but got: {given_header}"
 
         expected_payload = "eyJpc3MiOiJqb2UiLCJleHAiOjEzMDA4MTkzODAsImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ"
         payload = Payload({"iss": "joe", "exp": 1300819380, "http://example.com/is_root": True})
-
-        given_payload = payload.urlsafe_b64encode()
-
-        assert given_payload == expected_payload
+        given_payload = payload.encode()
+        assert given_payload == expected_payload, f"Expected payload: {expected_payload}, but got: {given_payload}"
 
         expected_signature = "Lk7xNLJ8ReSNz0O-cufFhk6CTJBVkR-0fY57B8LR62U"
-        signature = Signature("password", "HS256")
-        given_signature = signature.sign(header.urlsafe_b64encode(), payload.urlsafe_b64encode())
-
-        assert given_signature == expected_signature
-
-    def test_jwt_verification(self):
-        """
-        Scenario: JWT Verification
-
-        Given a JWT instance with a valid header, payload, and signature.
-        When verifying the JWT.
-        Then the result should be True, indicating that the JWT is valid.
-        """
-        pass
+        signature = SigningConfig("password", "HS256")
+        expected_jwt = f"{expected_header}.{expected_payload}.{expected_signature}"
+        given_jwt = JWT(header, payload, signature)
+        given_signature = given_jwt.sign()
+        assert given_signature == expected_signature, f"Expected signature: {expected_signature}, but got: {given_signature}"
+        assert given_jwt.encode() == expected_jwt, f"Expected JWT: {expected_jwt}, but got: {given_jwt.encode()}"
 
     def test_jwt_header_update(self):
         """
@@ -53,7 +41,20 @@ class TestJWT:
         When updating a claim in the header.
         Then the updated header claim should reflect the new value.
         """
-        pass
+        expected_header = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
+        header = Header("HS256", "JWT")
+        assert header.encode() == expected_header, f"Expected header: {expected_header}, but got: {header.encode()}"
+
+        expected_json_header = b'{"alg":"HS256","typ":"JWT"}'
+        assert header.to_json() == expected_json_header, f"Expected JSON header: {expected_json_header}, but got: {header.to_json()}"
+
+        header.add_claim("kid", "https://1password.invalid")
+
+        expected_new_header = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Imh0dHBzOi8vMXBhc3N3b3JkLmludmFsaWQifQ"
+        expected_new_json_header = b'{"alg":"HS256","typ":"JWT","kid":"https://1password.invalid"}'
+
+        assert header.encode() == expected_new_header, f"Expected new header: {expected_new_header}, but got: {header.encode()}"
+        assert header.to_json() == expected_new_json_header, f"Expected new JSON header: {expected_new_json_header}, but got: {header.to_json()}"
 
     def test_update_payload(self):
         """
@@ -63,27 +64,47 @@ class TestJWT:
         When updating the payload.
         Then the updated payload should reflect the new values.
         """
-        pass
+        initial_payload_data = {"iss": "joe", "exp": 1300819380, "http://example.com/is_root": True}
+        jwt_instance = JWT(
+            header=Header(alg="HS256", typ="JWT"),
+            payload=Payload(data=initial_payload_data),
+            signing_config=SigningConfig(key="secret_key", algorithm="HS256")
+        )
 
-    def test_none_signature(self):
+        jwt_instance.payload.add_claim("iat", 1516239022)
+
+        expected_updated_payload = "eyJpc3MiOiJqb2UiLCJleHAiOjEzMDA4MTkzODAsImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlLCJpYXQiOjE1MTYyMzkwMjJ9"
+        expected_updated_json_payload = b'{"iss":"joe","exp":1300819380,"http://example.com/is_root":true,"iat":1516239022}'
+
+        assert jwt_instance.payload.encode() == expected_updated_payload, f"Expected updated payload: {expected_updated_payload}, but got: {jwt_instance.payload.encode()}"
+        assert jwt_instance.payload.to_json() == expected_updated_json_payload, f"Expected updated JSON payload: {expected_updated_json_payload}, but got: {jwt_instance.payload.to_json()}"
+
+    @pytest.mark.usefixtures('valid_jwt_object')
+    def test_none_signature(self, valid_jwt_object):
         """
         Scenario: None Signature
 
-        Given a JWT instance with a valid header, payload, and no signature.
+        Given a JWT instance with a valid header, payload, and an alg of none, None, NoNe or NONE.
         When encoding the JWT.
         Then the encoded token should have no signature.
         """
         pass
 
-    def test_update_payload_without_changing_signature(self):
+    @pytest.mark.usefixtures('valid_jwt_object')
+    def test_update_payload_without_changing_signature(self, valid_jwt_object):
         """
         Scenario: Update Payload Without Changing Signature
 
         Given a JWT instance with a valid header, initial payload, and a secret key.
-        When updating the payload without changing the signature.
-        Then the encoded token should still be valid.
+        When updating the contents of the payload.
+        Then the signature should not change.
         """
-        pass
+        # Create a deep copy of the original JWT object
+        original_jwt_object = copy.deepcopy(valid_jwt_object)
+
+        valid_jwt_object.payload.add_claim("iat", 1516239022)
+
+        assert valid_jwt_object._signature == original_jwt_object._signature, f"Signature should not change when updating payload. Expected: {original_jwt_object._signature}, but got: {valid_jwt_object._signature}"
 
     def test_update_payload_and_change_signature(self):
         """
