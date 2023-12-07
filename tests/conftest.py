5fd139b6d3083bcd9411c09db4a3c4701594caf3
errbufferoverfl <gitlab@errbufferoverfl.me>
2023-12-08 09:00:26 +1100

    Add fixtures for new test cases

--- tests/conftest.py
+++ tests/conftest.py
@@ -33,6 +33,27 @@ def valid_jwt_object() -> JWT:
 
 @pytest.fixture
 def valid_jwt_string() -> str:
-    valid_jwt_path = Path("data/valid_jwt.txt")
-    with open(valid_jwt_path, "r") as file:
+    path = Path("data/valid_jwt.txt")
+    with open(path, "r") as file:
+        return file.read()
+
+
+@pytest.fixture
+def valid_jwt_no_sig_string() -> str:
+    path = Path("data/valid_jwt_no_sig.txt")
+    with open(path, "r") as file:
+        return file.read()
+
+
+@pytest.fixture
+def invalid_jwt_no_header() -> str:
+    path = Path("data/invalid_jwt_no_header.txt")
+    with open(path, "r") as file:
+        return file.read()
+
+
+@pytest.fixture
+def invalid_jwt_no_payload() -> str:
+    path = Path("data/invalid_jwt_no_payload.txt")
+    with open(path, "r") as file:
         return file.read()
